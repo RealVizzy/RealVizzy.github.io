99b78d15e4a87b766077f568f743a9d9d18b6e7a
ConarCZ <55970818+ConarCZ@users.noreply.github.com>
2021-01-14 00:59:50 +0000

    bottom text

---
+++ ideologies.js
@@ -0,0 +1,102 @@
+ideology = [
+	{
+		"ideology": ":stirner:",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Anti-Anti-Centrism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "BEism 2ism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Alphadonialism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "H Manism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Arctoism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Ethnic Welfarism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Left-Wing National Conservatism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Turkeyism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "National Keynesianism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Shaggerism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Jed-Pilled Thought",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Neo-Puritanism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "SajZeal Model",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Femboy Transhumanism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Left Liberalism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Tomassciism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Anarcho-Totalitarian Communism",
+		"flag": "flags/Anarcho-Totalitarian_Communism_Flag.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Ferolaism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	},
+	{
+		"ideology": "Edwinism",
+		"flag": "flags/placeholder.svg",
+		"desc": "BOTTOM TEXT"
+	}
+];
---
+++ questions.js
@@ -0,0 +1,254 @@
+questions = [
+    {
+        "question": "Self-Contradicting Believes",
+		"answers": "1",
+		"results": {
+			"a": "1",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "1",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Conservative",
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "2",
+			"b": "12",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Nihilism",
+		"answers": "1",
+		"results": {
+			"a": "2",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "3",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Socialist Larp",
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "4",
+			"b": "8",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Monarchy",
+		"answers": "1",
+		"results": {
+			"a": "3",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "5",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Capitalism",
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "4",
+			"c": "0"
+		},
+		"answer": {
+			"a": "6",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Technocracy",
+		"answers": "1",
+		"results": {
+			"a": "5",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "7",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Ethnostate",
+		"answers": "1",
+		"results": {
+			"a": "6",
+			"b": "7",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Capitalism", 
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "9",
+			"b": "10",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Darwinism", 
+		"answers": "1",
+		"results": {
+			"a": "8",
+			"b": "9",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Markets", 
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "10",
+			"c": "0"
+		},
+		"answer": {
+			"a": "11",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Communitarianism", 
+		"answers": "1",
+		"results": {
+			"a": "12",
+			"b": "11",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Capitalism", //finish
+		"answers": "1",
+		"results": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "13",
+			"b": "15",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Technocracy", 
+		"answers": "1",
+		"results": {
+			"a": "13",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "14",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Trap", 
+		"answers": "1",
+		"results": {
+			"a": "14",
+			"b": "15",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Mixed Mode of Distribution", 
+		"answers": "1",
+		"results": {
+			"a": "16",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "16",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Democracy vs ML", 
+		"answers": "1",
+		"results": {
+			"a": "17",
+			"b": "0",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "17",
+			"c": "0"
+        }
+    },
+    {
+        "question": "Transhumanism", 
+		"answers": "1",
+		"results": {
+			"a": "18",
+			"b": "19",
+			"c": "0"
+		},
+		"answer": {
+			"a": "0",
+			"b": "0",
+			"c": "0"
+        }
+    }
+];
