c8b3ed66a33506b296802117113af686d2a82559
ConarCZ <55970818+ConarCZ@users.noreply.github.com>
2021-01-29 01:45:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 starveDay = 25000 //Source : https://www.un.org/en/chronicle/article/losing-25000-hunger-every-day
 
+starveSecond = (((starveDay/24)/60)/60)
+
 var start = Date.now();
 
 setInterval(calcStarve, 500)
 
 function calcStarve() {
-    document.getElementById("starveCounter").innerHTML = Math.floor(((Date.now() - start)/1000)*(((starveDay/24)/60)/60))
+    document.getElementById("starveCounter").innerHTML = Math.floor(((Date.now() - start)/1000)*starveSecond)
 }
